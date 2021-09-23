840736a3a0b141d2cadfbf7de13cf8f34d37c7f9
hikaridesuyoo <andre123luiz.al@gmail.com>
2021-09-23 18:02:32 -0300

    Todas as propriedades aparecem na root

--- spec/system/visitor_view_properties_spec.rb
+++ spec/system/visitor_view_properties_spec.rb
@@ -1,32 +1,52 @@
 require 'rails_helper'
 
 describe 'Visitor visit homepage' do
-  it 'successfully sees existing properties' do
-    Property.create({ 
+  it 'and successfully sees existing properties' do
+
+    properties = []
+    properties << Property.create({ 
       title: 'titulo1', 
       description: 'descrição1',
       rooms: 3, 
       parking_available: true
     })
 
-    Property.create({ 
+    properties << Property.create({ 
       title: 'titulo2', 
       description: 'descrição2',
       rooms: 20, 
       parking_available: false
     })
 
+    properties << Property.create({ 
+      title: 'titulo2', 
+      description: 'descrição2',
+      parking_available: false
+    })
+
+    properties << Property.create({ 
+      title: 'casa em copa cabana', 
+      description: 'uma casa, em copa cabana',
+      parking_available: false,
+      allow_pets: true
+    })
+
     visit root_path
 
-    expect(page).to have_css('id#property-0', text: 'titulo1')
-    expect(page).to have_css('id#property-0', text: 'descrição1')
-    expect(page).to have_css('id#property-0', text: '3')
-    expect(page).to have_css('id#property-0', text: 'sim')
+    expect(page).to have_no_css('div#no_properties_note')
+    properties.each do |property|    
+      property_css_beginning = "div#property-#{property.id} > #"
+      expect(page).to have_css(property_css_beginning + 'title', text: property.title)
+      expect(page).to have_css(property_css_beginning + 'description', text: property.description)
+      expect(page).to have_css(property_css_beginning + 'rooms', text: property.rooms.to_s)
+      expect(page).to have_css(property_css_beginning + 'parking_available', text: property.parking_available ? 'sim' : 'não')
+    end
 
-    expect(page).to have_css('id#property-1', text: 'titulo2')
-    expect(page).to have_css('id#property-1', text: 'descrição2')
-    expect(page).to have_css('id#property-1', text: '20')
-    expect(page).to have_css('id#property-1', text: 'não')
+  end
 
+  it 'and sees a note if there are no properties' do
+    visit root_path
+
+    expect(page).to have_css('div#no_properties_note')
   end
 end
