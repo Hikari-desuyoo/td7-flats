24bb5336c6f11c2b82d02fc77f3ab9aa55d95af7
hikaridesuyoo <andre123luiz.al@gmail.com>
2021-09-28 15:45:41 -0300

    Adicionada validação de números

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,4 +3,7 @@ class Property < ApplicationRecord
     
     validates :title, :description, :rooms, :bathrooms, :daily_price,
               presence: { message: 'Obrigatório preencher' }
+
+    validates :rooms, :bathrooms, numericality: { only_integer: true, message: 'Necessário ser um número' }
+    validates :daily_price, numericality: { message: 'Necessário ser um número' }
 end
--- spec/system/visitor_register_property_spec.rb
+++ spec/system/visitor_register_property_spec.rb
@@ -43,6 +43,25 @@ describe 'Visitor visit homepage, clicks register' do
     expect(page).to have_content('Obrigatório preencher', count: 5)
   end
 
+  it 'and fills in text into integer fields and get error messages' do
+    PropertyType.create!(name: 'casa')
+    
+    visit root_path
+    find("#register_link").click
+    fill_in 'property_title', with: 'titulo1'
+    fill_in 'property_description', with: 'descrição1'
+    select 'casa', from: 'property_property_type_id'
+    check 'property_allow_pets'
+    check 'property_parking_available'
+
+    fill_in 'property_rooms', with: 'a'
+    fill_in 'property_bathrooms', with: 5.2
+    fill_in 'property_daily_price', with: 'c'
+    click_on 'commit'
+
+    expect(page).to have_content('Necessário ser um número', count: 3)
+  end
+
   it 'and goes back to menu' do
     visit root_path
     find("#register_link").click
