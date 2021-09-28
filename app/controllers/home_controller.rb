5fc0c4fef2c3c1cde3dba294d54cf835f751c496
hikaridesuyoo <andre123luiz.al@gmail.com>
2021-09-28 19:34:42 -0300

    Implementado filtrar por regiao

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,10 @@ class HomeController < ApplicationController
         @properties = Property.all
         
         @selected_property_type_id = -1
+        @selected_property_location_id = -1
 
         @property_types_selection_list = property_types_selection_list
+        @property_locations_selection_list = property_locations_selection_list
        
     end
 
@@ -13,25 +15,45 @@ class HomeController < ApplicationController
         @filtered = true
 
         @selected_property_type_id = params[:property_type_filter].to_i
+        @selected_property_location_id = params[:property_location_filter].to_i
+
+
+        @properties = Property.all
 
         unless @selected_property_type_id == -1
-            selected_property_type = PropertyType.find(@selected_property_type_id)
-            @properties = selected_property_type.properties
-        else
-            @properties = Property.all
+            @properties = @properties.where(
+                property_type: PropertyType.find(@selected_property_type_id)
+            )
+        end
+
+        unless @selected_property_location_id == -1
+            @properties = @properties.where(
+                property_location: PropertyLocation.find(@selected_property_location_id)
+            )
         end
         
 
         @property_types_selection_list = property_types_selection_list
+        @property_locations_selection_list = property_locations_selection_list
+
 
         render :index
     end
 
     def property_types_selection_list
-        property_types = PropertyType.all
-        selection_list = property_types.map {|p_type| [p_type.name, p_type.id]}
+        all = PropertyType.all
+        selection_list = all.map {|model_obj| [model_obj.name, model_obj.id]}
         selection_list = [['QUALQUER UM', -1]] + selection_list
         selection_list
     end 
+
+    def property_locations_selection_list
+        all = PropertyLocation.all
+        selection_list = all.map {|model_obj| [model_obj.name, model_obj.id]}
+        selection_list = [['QUALQUER UM', -1]] + selection_list
+        selection_list
+    end 
+
+
 
 end
---
+++ spec/system/visitor_filters_properties_by_location_spec.rb
@@ -0,0 +1,180 @@
+require 'rails_helper'
+
+
+describe 'Visitor visit homepage, filters by property location' do
+    it 'and filters by nothing(show all properties normally)' do
+      #arrange
+      pousada = PropertyType.create!(name: 'pousada')
+
+      rio = PropertyLocation.create!(name: 'rio')
+      minas = PropertyLocation.create!(name: 'minas')
+
+      properties = []
+
+      properties << Property.create!({ 
+        title: 'titulo1', 
+        description: 'descrição1',
+        rooms: 3, 
+        bathrooms: 7,
+        daily_price: 400,
+        property_type: pousada,
+        property_location: rio,
+        parking_available: true
+      })
+    
+      properties << Property.create!({ 
+        title: 'titulo2', 
+        description: 'descrição2',
+        rooms: 20, 
+        bathrooms: 2,
+        daily_price: 401,
+        property_type: pousada,
+        property_location: minas,
+        parking_available: false
+      })
+
+      #act
+      visit root_path
+      within('div#filtering') do
+        click_on 'commit'
+      end
+
+      #assert
+      within('div#properties') do
+          property_css_beginning = 'div#property-'
+          expect(page).to have_css(property_css_beginning+"#{properties[0].id}")
+          expect(page).to have_css(property_css_beginning+"#{properties[1].id}")
+      end
+
+    end
+
+    it 'and have no results after filtering' do
+        pousada = PropertyType.create!(name: 'pousada')
+
+        rio = PropertyLocation.create!(name: 'rio')
+        minas = PropertyLocation.create!(name: 'minas')
+
+        properties = []
+        properties << Property.create!({ 
+          title: 'titulo1', 
+          description: 'descrição1',
+          rooms: 3, 
+          bathrooms: 7,
+          daily_price: 400,
+          property_type: pousada,
+          property_location: minas,
+          parking_available: true
+        })
+      
+        properties << Property.create!({ 
+          title: 'titulo2', 
+          description: 'descrição2',
+          rooms: 20, 
+          bathrooms: 2,
+          daily_price: 401,
+          property_type: pousada,
+          property_location: minas,
+          parking_available: false
+        })
+      
+        properties << Property.create!({ 
+          title: 'titulo2', 
+          description: 'descrição2',
+          rooms: 2, 
+          bathrooms: 9,
+          daily_price: 402,
+          property_type: pousada,
+          property_location: minas,
+          parking_available: false
+        })
+      
+        properties << Property.create!({ 
+          title: 'casa em copacabana', 
+          description: 'uma casa, em copacabana',
+          rooms: 3, 
+          parking_available: false,
+          bathrooms: 32,
+          daily_price: 403,
+          property_type: pousada,
+          property_location: minas,
+          allow_pets: true
+        })
+
+        visit root_path
+        within('div#filtering') do
+            select 'rio', from: 'property_location_filter'
+            click_on 'commit'
+        end
+
+        expect(page).to have_css('div#no_properties_note')
+
+    end
+
+    it 'and successfully filters existing properties' do
+        properties = []
+
+        casa = PropertyType.create!(name: 'casa')
+
+        rio = PropertyLocation.create!(name: 'rio')
+        minas = PropertyLocation.create!(name: 'minas')
+        sp = PropertyLocation.create!(name: 'sao paulo')
+    
+        properties << Property.create!({ 
+          title: 'titulo1', 
+          description: 'descrição1',
+          rooms: 3, 
+          bathrooms: 7,
+          daily_price: 400,
+          property_type: casa,
+          property_location: rio,
+          parking_available: true
+        })
+      
+        properties << Property.create!({ 
+          title: 'titulo2', 
+          description: 'descrição2',
+          rooms: 20, 
+          bathrooms: 2,
+          daily_price: 401,
+          property_type: casa,
+          property_location: minas,
+          parking_available: false
+        })
+      
+        properties << Property.create!({ 
+          title: 'titulo2', 
+          description: 'descrição2',
+          rooms: 2, 
+          bathrooms: 9,
+          daily_price: 402,
+          property_type: casa,
+          property_location: sp,
+          parking_available: false
+        })
+      
+        properties << Property.create!({ 
+          title: 'casa em copacabana', 
+          description: 'uma casa, em copacabana',
+          rooms: 3, 
+          parking_available: false,
+          bathrooms: 32,
+          property_location: sp,
+          daily_price: 403,
+          property_type: casa,
+          allow_pets: true
+        })
+
+        visit root_path
+        select 'sao paulo', from: 'property_location_filter'
+        click_on 'commit'
+
+        within('div#properties') do
+            property_css_beginning = 'div#property-'
+            expect(page).to_not have_css(property_css_beginning+"#{properties[0].id}")
+            expect(page).to_not have_css(property_css_beginning+"#{properties[1].id}")
+            expect(page).to have_css(property_css_beginning+"#{properties[2].id}")
+            expect(page).to have_css(property_css_beginning+"#{properties[3].id}")
+        end
+
+    end
+end
